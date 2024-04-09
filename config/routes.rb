e336626e641458d7d3035dc5350b40beb2e1da29
Spencer Lewis <spence.914@gmail.com>
2024-04-09 16:21:40 -0700

    add admin/categories page, add link in top nav, add link in admin/dashboard, add functionality to create new categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   
+
   get 'about', to: 'about#index'
   root to: 'products#index'
 
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create] 
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
