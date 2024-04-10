e23b9bf966c4d58bee4eea4f4a5c550251283ffa
Spencer Lewis <spence.914@gmail.com>
2024-04-10 15:26:43 -0700

    add user notices for failed logins, successful logouts using flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,12 +12,15 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      flash[:alert] = 'Invalid email or password.'
       redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    
     redirect_to '/login'
+    flash[:notice] = 'Logged Out.'
   end
 end
