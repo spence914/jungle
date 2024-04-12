37bbc195c000c1433cdeaace9c1491413e487274
Spencer Lewis <spence.914@gmail.com>
2024-04-12 13:20:28 -0700

    clean up comments, whitespace, unneccessary files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
 
@@ -30,8 +28,7 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
-    # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
   def current_user
