d61b8d6c55635002cda7c8b45caaadad54a1fc96
Spencer Lewis <spence.914@gmail.com>
2024-04-23 14:49:02 -0700

    Add tests for home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,12 @@
+it('can visit the home page', () => {
+  
+  cy.visit("http://localhost:3000/")
+})
+
+it("There is products on the page", () => {
+  cy.get(".products article").should("be.visible");
+});
+
+it("There is 2 products on the page", () => {
+  cy.get(".products article").should("have.length", 2);
+});
